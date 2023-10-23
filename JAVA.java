336838cc9d4a0919365ab8b5e31b9a297adc5175
Aditya Mistri <123372823+aditya-mistri@users.noreply.github.com>
2023-10-23 15:26:52 +0530

    Update JAVA.java

--- JAVA.java
+++ JAVA.java
@@ -2,6 +2,10 @@
 
 class HelloWorld {
     public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int sum = a+b;
+        System.out.println(sum);
         System.out.println("Hello, World!"); 
     }
 }
